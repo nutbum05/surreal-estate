992c1d61a00b8f4227d968e1d661374ed9298f5a
nutbum05 <lukasz.orzech@gmail.com>
2019-08-31 19:52:17 +0100

    added addproperty component

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/AddProperty.css';
 
-const AddProperty = () => <div> Add Property PAge </div>;
+class AddProperty extends Component {
+ state = {
+   fields: {
+     title: '',
+   },
+ };
+
+    handleAddProperty = (event) => {
+      event.preventDefault();
+    };
+
+    handleFieldChange = event => {
+      this.setState({
+        fields: {
+          ...this.state.fields,
+          [event.target.name]: event.target.value,
+        },
+      });
+    };
+
+    render() {
+      return (
+        <div className="AddProperty">
+          <form onSubmit={this.handleAddProperty}>
+            <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
+          </form>
+        </div>
+      );
+    }
+}
 
 export default AddProperty;
